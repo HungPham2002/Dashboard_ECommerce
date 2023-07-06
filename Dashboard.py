987c7ee92ff524856fdfeaff85aae63ab9410d49
HungPham2002 <phamvinhhung02@gmail.com>
2023-07-06 10:50:39 +0700

    modified something

--- Dashboard.py
+++ Dashboard.py
@@ -17,7 +17,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 # import time
 
-# Tạo Dashboard
+# create a Dashboard
 
 # Using "with" notation
 with st.sidebar:
@@ -28,14 +28,14 @@ with st.sidebar:
     st.image(image_2)
     col1,col2 = st.columns(2)
     with col1:
-        st.metric(label="Temperature", value="31 °C", delta="1.0 °C",
+        st.metric(label="Temperature", value="30 °C", delta="1.0 °C",
         delta_color="inverse")
     with col2:
-        st.metric(label="Wind", value="9 mph", delta="-8%",
+        st.metric(label="Wind", value="15 km/h", delta="-8%",
         delta_color="inverse")
     col1,col2 = st.columns(2)
     with col1:
-        st.metric(label="Humidity", value="15%", delta="-4%",
+        st.metric(label="Humidity", value="73%", delta="-4%",
         delta_color="inverse")
     with col2:
         st.metric(label="Location", value="HCM",
@@ -44,7 +44,6 @@ with st.sidebar:
         st.subheader("Students:")
         st.text("Phạm Hữu Hùng - 20521371")
         st.text("Lê Văn Duy - 20521233")
-        st.text("Nguyễn Phú Kiệt - 21522257")
         
 
 st.title("Customer Churn Dashboard In 2022")
@@ -54,11 +53,11 @@ st.balloons()
 # Load data
 file = "telecom_customer_churn_datechurn.csv"
 df = pd.read_csv(file)
-# Biểu đồ 1
+# Diagram 1
 df['Churn Date'] = pd.to_datetime(df['Churn Date'])
 monthly_churn = df.groupby(pd.Grouper(key='Churn Date', freq='M')).size() / len(df)
 
-# Biểu đồ 2
+# Diagram 2
 big_cities = ['San Francisco', 'Los Angeles', 'San Diego', 'San Jose', 'Sacramento', 'Fresno', 'Long Beach', 'Oakland', 'Santa Ana', 'Anaheim', 'Riverside', 'Stockton']
 df.loc[df['City'].isin(big_cities), 'city_group'] = 'Big cities'
 medium_cities = ['Bakersfield', 'Chula Vista', 'Modesto', 'Irvine', 'Glendale', 'Fremont', 'San Bernardino', 'Fontana', 'Moreno Valley', 'Huntington Beach', 'Santa Clarita']
@@ -68,12 +67,12 @@ df.loc[df['City'].isin(small_cities), 'city_group'] = 'Small cities'
 
 
 
-# Biểu đồ 4: Tỉ lệ churn theo độ tuổi
+# Diagram 4: Prop churn by age
 age_churn_count = df[df['Customer Status'] == 'Churned'].groupby('Age')['Customer ID'].count()
 age_total_count = df.groupby('Age')['Customer ID'].count()
 age_churn_rate = age_churn_count / age_total_count
 
-# Vẽ biểu đồ 1 & 2 & 4:
+# Draw Diagram 1 & 2 & 4:
 
 with st.container():
     col1, col2, col3 = st.columns(3)
@@ -100,17 +99,17 @@ with st.container():
         st.pyplot(plt)
         
 
-# Biểu đồ 3
+# Diagram 3
 gender_churn_count = df[df['Customer Status'] == 'Churned'].groupby('Gender')['Customer ID'].count()
 gender_total_count = df.groupby('Gender')['Customer ID'].count()
 gender_churn_rate = gender_churn_count / gender_total_count
-# Biểu đồ 6: Tỉ lệ khách hàng theo tình trạng khách hàng
+# Biểu đồ 6: Prop customer status
 status_count = df['Customer Status'].value_counts()
-# Biểu đồ 7: Tỷ lệ churn theo từng danh mục churn
+# Diagram 7: Prop churn by churn category
 churn_by_category = df[df['Customer Status'] == 'Churned']['Churn Category'].value_counts()
 
-# Vẽ Biểu đồ 3 & 6 & 7
-# Tạo visual với kích thước và vị trí tùy chỉnh
+# Draw Diagram 3 & 6 & 7
+# create visual with size and position custom
 with st.container():
     grid = st.container()
     with grid:
@@ -135,7 +134,7 @@ with st.container():
             st.pyplot(fig)
             
         
-# Biểu đồ 5: Số lượng khách hàng theo từng loại hợp đồng và tình trạng khách hàng
+# Diagram 5: the number of customer displayed by contracts & customer status
 contract_churn_count = df.groupby(['Contract', 'Customer Status'])['Customer ID'].count().unstack()
 with st.container():
     fig, ax = plt.subplots()
@@ -145,19 +144,19 @@ with st.container():
     ax.set_title('Customer Churn by Contract Type')
     ax.legend(title='Customer Status', loc='center left', bbox_to_anchor=(1, 0.5))
     st.pyplot(fig)
-# biểu đồ map
+#  map (Just random, LOL)
 with st.container():
-    random_matrix_1 = np.random.randn(100, 2) / [3, 1] + [36.782610 , -119.579324]
-    random_matrix_2 = np.random.randn(100, 2) / [2, 1] + [39.782610 , -121.579324]
-    random_matrix_3 = np.random.randn(100, 2) / [2, 1] + [37.782610 , -120.579324]
+    random_matrix_1 = np.random.randn(200, 2) / [3, 1] + [36.782610 , -119.579324]
+    random_matrix_2 = np.random.randn(200, 2) / [2, 1] + [39.782610 , -121.579324]
+    random_matrix_3 = np.random.randn(200, 2) / [2, 1] + [37.782610 , -120.579324]
     # San Francisco
-    random_matrix_4 = np.random.randn(100, 2) / [50, 50] + [37.76, -122.4]
+    random_matrix_4 = np.random.randn(200, 2) / [50, 50] + [37.76, -122.4]
     # Los Angeles
-    random_matrix_5 = np.random.randn(100, 2) / [50, 50] + [34.0522342, -118.2436849]
+    random_matrix_5 = np.random.randn(200, 2) / [50, 50] + [34.0522342, -118.2436849]
     # San Diego
-    random_matrix_6 = np.random.randn(100, 2) / [50, 50] + [32.715738, -117.1610838]
-    random_matrix_7 = np.random.randn(100, 2) / [2, 1] + [35.0522342, -118.2436849]
-    random_matrix_8 = np.random.randn(100, 2) / [1, 1] + [34.0522342, -116.2436849]
+    random_matrix_6 = np.random.randn(200, 2) / [50, 50] + [32.715738, -117.1610838]
+    random_matrix_7 = np.random.randn(200, 2) / [2, 1] + [35.0522342, -118.2436849]
+    random_matrix_8 = np.random.randn(200, 2) / [1, 1] + [34.0522342, -116.2436849]
     df = pd.DataFrame(np.concatenate((random_matrix_1, random_matrix_2, random_matrix_3, random_matrix_4, random_matrix_5, random_matrix_6, random_matrix_7, random_matrix_8)),
                     columns=['lat', 'lon'])
     st.map(df)
@@ -182,7 +181,7 @@ scaler = StandardScaler()
 X_train[col] = StandardScaler().fit_transform(X_train[col])
 X_test[col] = StandardScaler().fit_transform(X_test[col])
 
-tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["KNN", "Logistic Regression", "SVM", "Decision Tree", "Random Forest", "Artificial Neural Network"])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["KNN", "Logistic Regression", "SVM", "Decision Tree", "Random Forest", ])
 with tab1:
     # 1. KNN MODEL
     # Train model
@@ -210,7 +209,6 @@ with tab1:
     roc_auc = auc(fpr, tpr)
     plt.figure()
     plt.plot(fpr, tpr, color='darkorange', lw=2, label='KNeighborsClassifier (AUC = %0.2f)' % roc_auc)
-    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Rate')
@@ -263,7 +261,6 @@ with tab2:
     # ROC
     figL, axL = plt.subplots()
     axL.plot(fprL, tprL, color='darkorange', lw=2, label='LogisticRegression (AUC = %0.2f)' % roc_aucL)
-    axL.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
     axL.set_xlim([0.0, 1.0])
     axL.set_ylim([0.0, 1.05])
     axL.set_xlabel('False Positive Rate')
@@ -315,7 +312,6 @@ with tab3:
     # ROC
     plt.figure()
     plt.plot(fpr, tpr, color='darkorange', lw=2, label='SVM (AUC = %0.2f)' % roc_auc)
-    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Rate')
@@ -366,7 +362,6 @@ with tab4:
     # ROC
     plt.figure()
     plt.plot(fpr, tpr, color='darkorange', lw=2, label='DecisionTreeClassifier (AUC = %0.2f)' % roc_auc)
-    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Rate')
@@ -394,7 +389,7 @@ with tab4:
 with tab5:
     # Random Forest model
     max_features = 15
-    max_leaf_nodes = 16 
+    max_leaf_nodes = 24
     n_estimators = 50
     # Train model
     good_model = RandomForestClassifier(max_leaf_nodes = max_leaf_nodes,
@@ -421,7 +416,6 @@ with tab5:
     # ROC
     plt.figure()
     plt.plot(fpr, tpr, color='darkorange', lw=2, label='RandomForest (AUC = %0.2f)' % roc_auc)
-    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Rate')
@@ -444,19 +438,3 @@ with tab5:
     with col2:
         st.write("### ROC Curve")
         st.pyplot(plt)
-with tab6:
-    # display on streamlit
-    st.write("## Artificial Neural Network Model Performance")
-    col1, col2 = st.columns([1,2])
-    with col1:
-        st.write("### Evaluate Model")
-        st.write(f"ROC AUC = 0.916")
-        st.write(f"Valid accuracy = 0.846")
-        st.write(f"Test precision = 0.882")
-        st.write(f"F1-Score = 0.894")
-    with col2:   
-        
-        image_3 = Image.open('ANN.png')
-        st.image(image_3)
-
-
